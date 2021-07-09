4745e5b515a29187a2da893634a668a37a4a0096
Raghav Dwivedi <glgraghav@gmail.com>
2021-07-09 18:46:17 +0400

    Records and saves

--- src/render.js
+++ src/render.js
@@ -2,10 +2,25 @@ const videoElement = document.querySelector('video');
 const startBtn = document.getElementById('startBtn');
 const stopBtn = document.getElementById('stopBtn');
 const videoSelectBtn = document.getElementById('videoSelectBtn');
-videoSelectBtn.onclick = getVideoSources;
 
 const { desktopCapturer, remote } = require('electron');
-const { Menu } = remote;
+const { Menu, dialog } = remote;
+const { writeFile } = require('fs');
+
+let mediaRecorder;
+const recordedChunks = [];
+
+videoSelectBtn.onclick = getVideoSources;
+startBtn.onclick = e => {
+    mediaRecorder.start();
+    startBtn.classList.add('is-danger');
+    startBtn.innerText = 'Recording';
+}
+stopBtn.onclick = e => {
+    mediaRecorder.stop();
+    startBtn.classList.remove('is-danger');
+    startBtn.innerText = 'Start';
+}
 
 async function getVideoSources() {
     const inputSources = await desktopCapturer.getSources({
@@ -41,4 +56,30 @@ async function selectSource(source) {
 
     videoElement.srcObject = stream;
     videoElement.play();
+
+    const options = { mimeType: 'video/webm; codecs=vp9' };
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.onstop = handleStop;
+
+    function handleDataAvailable(e) {
+        console.log('video data available');
+        recordedChunks.push(e.data);
+    }
+}
+
+async function handleStop(e) {
+    const blob = new Blob(recordedChunks, { type: 'video/webm; codecs=vp9' });
+
+    const buffer = Buffer.from(await blob.arrayBuffer());
+
+    const { filePath } = await dialog.showSaveDialog({
+        buttonLabel: 'Save video',
+        defaultPath: `vid-${Date.now()}.webm`
+    });
+
+    console.log(filePath);
+
+    writeFile(filePath, buffer, () => console.log('video saved successfully!'));
 }
